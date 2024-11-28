11664c161dcb69def8328b5ba7a40a09f9817ea0
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-11-28 22:12:36 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,11 +1,12 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.Buffer;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Task03Main {
 
@@ -19,6 +20,17 @@ public class Task03Main {
     }
 
     public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+        TreeMap<String, TreeSet<String>> result = new TreeMap<>();
+        List<String> input = new BufferedReader(new InputStreamReader(inputStream, charset)).lines()
+                .map(String::toLowerCase)
+                .filter(letter -> letter.matches("[а-яё]+") && letter.length() >= 3)
+                .collect(Collectors.toList());
+        for (String line : input) {
+            char[] chars = line.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            result.computeIfAbsent(key, k -> new TreeSet<>()).add(line);
+        }
+        return result.values().stream().filter(list -> list.size() > 1).collect(Collectors.toList());
     }
 }
